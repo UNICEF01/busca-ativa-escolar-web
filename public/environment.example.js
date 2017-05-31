96d14f3e1831c28067dffd948a8afd4f3ce01c5f
Aryel Tupinambá <aryel.tupinamba@lqdi.net>
2017-05-31 16:22:21 -0300

    Environments: added support to server-defined environments (determines default endpoint)

---
+++ public/environment.example.js
@@ -0,0 +1,4 @@
+window.ENVIRONMENT = {
+	SERVER_NAME: "Local Front-end",
+	DEFAULT_ENDPOINT: 'local_http'
+};
--- public/js/app.js
+++ public/js/app.js
@@ -38,6 +38,17 @@
 			numeral.language('pt-br');
 			moment.locale('pt-br');
 
+			function env(key) {
+				if(!window.ENVIRONMENT) {
+					window.ENVIRONMENT = {
+						SERVER_NAME: "Default (not configured)",
+						DEFAULT_ENDPOINT: 'prod_https'
+					};
+				}
+
+				return window.ENVIRONMENT[key];
+			}
+
 			var config = {
 
 				BUILD_PREFIX: 'b060.', // @DEPRECATED: see config/local_storage.js instead!
@@ -49,9 +60,14 @@
 						token: 'http://api.busca-ativa-escolar.local/api/auth/token',
 					},
 					homolog_http: {
-						label: 'V1 Homolog - web4-lqdi (Insecure)',
+						label: 'V1 Homolog - web3-lqdi (Insecure)',
 						api: 'http://api.busca-ativa-escolar.dev.lqdi.net/api/v1/',
 						token: 'http://api.busca-ativa-escolar.dev.lqdi.net/api/auth/token',
+					},
+					tests_http: {
+						label: 'V1 Tests - conexaobrasil (Insecure)',
+						api: 'http://api.buscaativateste.conexaobrasil.org/api/v1/',
+						token: 'http://api.buscaativateste.conexaobrasil.org/api/auth/token',
 					},
 					prod_http: {
 						label: 'V1 Prod - buscaativaescolar-web1 (Insecure)',
@@ -70,10 +86,12 @@
 				TOKEN_EXPIRES_IN: 3600, // 1 hour
 				REFRESH_EXPIRES_IN: 1209600, // 2 weeks
 
-				ALLOWED_ENDPOINTS: ['local_http', 'homolog_http', 'prod_http', 'prod_https'],
-				CURRENT_ENDPOINT: 'prod_https'
+				ALLOWED_ENDPOINTS: ['local_http', 'homolog_http', 'tests_http', 'prod_http', 'prod_https'],
+				CURRENT_ENDPOINT: env('DEFAULT_ENDPOINT')
 
 			};
+
+			console.log("[core.config] Current environment: ", window.ENVIRONMENT.SERVER_NAME);
 
 			var hasCheckedCookie = false;
 
