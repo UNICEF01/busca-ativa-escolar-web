5ba2153097c54737b782e76188cabe66dc2fad1e
SANDY SANTOS <sandysk8@gmail.com>
2018-11-16 20:10:13 -0200

    Tradução

--- resources/assets/js/children/child_browser.js
+++ resources/assets/js/children/child_browser.js
@@ -6,14 +6,14 @@
 				url: '/children',
 				templateUrl: '/views/children/browser.html',
 				controller: 'ChildSearchCtrl'
-			})
+			});
 		})
-		.controller('ChildSearchCtrl', function ($scope, $anchorScroll, $httpParamSerializer, API, Children, Decorators, Modals) {
+		.controller('ChildSearchCtrl', function ($scope, $anchorScroll, $httpParamSerializer, API, Children, Decorators, Modals, DTOptionsBuilder, DTColumnBuilder) {
 
 			$scope.Decorators = Decorators;
 			$scope.Children = Children;
 
-			$scope.defaultQuery = {
+          $scope.defaultQuery = {
 				name: '',
 				step_name: '',
 				cause_name: '',
@@ -48,12 +48,35 @@
 				Children.export($scope.query, function (res) {
 					console.log("Exported: ", res);
 					Modals.show(Modals.DownloadLink('Baixar arquivo XLS', 'Clique no link abaixo para baixar os casos exportados:', res.download_url));
-				})
+				});
 			};
-
 			$scope.refresh();
 
+          var language = {
+            "sEmptyTable": "Nenhum registro encontrado",
+            "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
+            "sInfoFiltered": "(Filtrados de _MAX_ registros)",
+            "sInfoPostFix": "",
+            "sInfoThousands": ".",
+            "sLengthMenu": "_MENU_ resultados por página",
+            "sLoadingRecords": "Carregando...",
+            "sProcessing": "Processando...",
+            "sZeroRecords": "Nenhum registro encontrado",
+            "sSearch": "Pesquisar",
+            "oPaginate": {
+              "sNext": "Próximo",
+              "sPrevious": "Anterior",
+              "sFirst": "Primeiro",
+              "sLast": "Último"
+            },
+            "oAria": {
+              "sSortAscending": ": Ordenar colunas de forma ascendente",
+              "sSortDescending": ": Ordenar colunas de forma descendente"
+            }
+          }
 
+          $scope.dtOptions = DTOptionsBuilder.newOptions()
+          .withLanguage(language);
 		});
-
 })();
