1e9b4714940ae80c65c1b7297201b2f60e7d5c8c
Manoel Souza <mansouza@unicef.org>
2019-03-11 10:01:58 -0300

    Paginacao das escolas, persmissao para envio apenas se preechidos

--- resources/assets/js/schools/school_browser.js
+++ resources/assets/js/schools/school_browser.js
@@ -12,34 +12,29 @@
                    
 			$scope.check_all_schools = false;
 			$scope.identity = Identity;
-			$scope.schools = [];
-			
+			$scope.schools = {};
+			$scope.msg_success = false;
+			$scope.msg_error = false;
+			$scope.query = {
+				sort: {},
+				show_suspended: false,
+				max: 5,
+				page: 1
+			};
 			$scope.selected = {
 				schools: []
 			};
-
-			$scope.query = {
-				filter: {},
-				sort: {},
-				max: 16,
-				page: 1
-			};
-			
-			$scope.msg_success = false;
-			$scope.msg_error = false;
 			
 			$scope.onCheckSelectAll = function(){
 				if( $scope.check_all_schools ){
-					$scope.selected.schools = angular.copy($scope.schools);			
+					$scope.selected.schools = angular.copy($scope.schools.data);
 				}else{
 					$scope.selected.schools = [];
 				}
 			};
 
-            $scope.onChangeSchool = function(school){
-                var school_in_array = $scope.schools.find( function(element_in_array) { return element_in_array.id === school.id } );
-                school_in_array = school;
-                Schools.update(school_in_array).$promise.then($scope.onSaved);
+            $scope.onModifySchool = function(school){
+                Schools.update(school).$promise.then($scope.onSaved);
             };
 			
 			$scope.onSaved = function(res) {
@@ -51,44 +46,39 @@
 			};
 
 			$scope.sendnotification = function(){
-				
-				Modals.show(
-					Modals.ConfirmEmail(
-						'Confirma o envio de sms e email para as seguintes escolas?',
-						'Ao confirmar, as escolas serão notificadas por email e sms e poderão cadastrar o endereço das crianças e adolescentes reportadas pelo Educacenso',
-						$scope.selected.schools
-					)).then(function () {
-						return Schools.send_notifications($scope.selected.schools).$promise;
-					})
-					.then(function (res) {
-						
-						if(res.status == "error"){
 
-							ngToast.danger(res.message);
-							$scope.msg_success = false;
-							$scope.msg_error = true;
-
-							$scope.refresh();
-							window.scrollTo(0, 0);
-							
-						}else{
-	
-							ngToast.success(res.message);
-							$scope.msg_success = true;
-							$scope.msg_error = false;
-
-							$scope.refresh();
-							window.scrollTo(0, 0);
-						}
-
-						
-					});
+				if($scope.selected.schools.length > 0){
+					
+					Modals.show(
+						Modals.ConfirmEmail(
+							'Confirma o envio de sms e email para as seguintes escolas?',
+							'Ao confirmar, as escolas serão notificadas por email e sms e poderão cadastrar o endereço das crianças e adolescentes reportadas pelo Educacenso',
+							$scope.selected.schools
+						)).then(function () {
+							return Schools.send_notifications($scope.selected.schools).$promise;
+						})
+						.then(function (res) {
+							if(res.status == "error"){
+								ngToast.danger(res.message);
+								$scope.msg_success = false;
+								$scope.msg_error = true;
+								$scope.refresh();
+								window.scrollTo(0, 0);
+							}else{
+								ngToast.success(res.message);
+								$scope.msg_success = true;
+								$scope.msg_error = false;
+								$scope.refresh();
+								window.scrollTo(0, 0);
+							}
+						});
+				}
 
 			};
 
 			$scope.refresh = function() {                
-                Schools.all_educacenso(function(res) {
-					$scope.schools = angular.copy(res.data);	
+                Schools.all_educacenso($scope.query, function(res) {
+					$scope.schools = angular.copy(res);
 				});
 			};
 			
