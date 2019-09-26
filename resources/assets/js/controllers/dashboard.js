42a8c22726fd77b55ffb47bd29e343d1a4b30d6a
Sandy <ssantos@unicef.org>
2019-09-26 12:26:01 -0300

    BAE: ajustes finais (HTML + CSS) - Relacionada com feature #296#317 Aparecer nos perfis coordenador operacional agente comunitario supervisor institucional tecnico verificador

--- resources/assets/js/controllers/dashboard.js
+++ resources/assets/js/controllers/dashboard.js
@@ -7,9 +7,7 @@
         $scope.tenantInfo = Tenants.getSettings();
 
         $scope.ready = false;
-
-        $scope.showInfo = 1;
-
+        $scope.showInfo = '';
         $scope.steps = [
             {name: 'Adesão', info: ''},
             {name: 'Configuração', info: ''},
@@ -22,21 +20,17 @@
 
             if (data.status !== 'ok') {
                 $scope.steps[0].info = data.bar.registered_at && data.bar.registered_at.date || 0;
-                $scope.steps[1].info = data.bar.config.updated_at && data.bar.config.updated_at.date || 0;
-                $scope.steps[2].info = 0;
-                $scope.steps[3].info = 0;
-                $scope.steps[4].info = 0;
+                $scope.steps[1].info = data.bar.config.updated_at.date || 0;
+                $scope.steps[2].info = data.bar.first_alert.date || 0;
+                $scope.steps[3].info = data.bar.first_case.date || 0;
+                $scope.steps[4].info = data.bar.first_reinsertion_class.date || 0;
                 $scope.otherData = data;
 
-                for (var i = 0; $scope.steps.length > i; i++) {
+                for (var i = 0; $scope.steps.length >= i; i++) {
                     var actualDate = moment($scope.steps[i].info);
-                    if (!actualDate._i && $scope.showInfo == 1) {
+                    if (actualDate._i === 0 && $scope.showInfo === '') {
                         $scope.showInfo = i;
                     }
-                    var finish = _.includes($scope.steps[i].info, 0);
-                }
-                if (finish) {
-                    $scope.showInfo = undefined
                 }
             }
         });
