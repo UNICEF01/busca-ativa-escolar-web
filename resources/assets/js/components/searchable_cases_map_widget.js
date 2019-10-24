d0172a4e7fd005199e4bdc8ce988f071892537c7
Sandy <ssantos@unicef.org>
2019-10-24 16:32:01 -0300

    Inserção de Textos das etapas#351

--- resources/assets/js/components/searchable_cases_map_widget.js
+++ resources/assets/js/components/searchable_cases_map_widget.js
@@ -1,85 +1,83 @@
-(function() {
+(function () {
 
-	angular.module('BuscaAtivaEscolar').directive('searchableCasesMap', function (moment, $timeout, uiGmapGoogleMapApi, Utils, Identity, Platform, Children, StaticData) {
+    angular.module('BuscaAtivaEscolar').directive('searchableCasesMap', function (moment, $timeout, uiGmapGoogleMapApi, Utils, Identity, Platform, Children, StaticData) {
 
-		function init(scope, element, attrs) {
+        function init(scope, element, attrs) {
 
-			scope.clustererOptions = {
-				imagePath: '/images/clusterer/m'
-			};
+            scope.clustererOptions = {
+                imagePath: '/images/clusterer/m'
+            };
 
-			scope.ctrl = {
-				events: {
-					tilesloaded: function(map) {
-						scope.ctrl.map = map;
-					}
-				}
-			};
+            scope.ctrl = {
+                events: {
+                    tilesloaded: function (map) {
+                        scope.ctrl.map = map;
+                    }
+                }
+            };
 
-			scope.onSearch = function(givenUf, givenCity) {
-				console.log("[widget.searchable_cases_map] Searching for: ", givenUf, givenCity);
+            scope.onSearch = function (givenUf, givenCity) {
+                console.log("[widget.searchable_cases_map] Searching for: ", givenUf, givenCity);
 
-				var uf = Utils.search(StaticData.getUFs(), function (uf) {
-					return (uf.code === givenUf);
-				});
+                var uf = Utils.search(StaticData.getUFs(), function (uf) {
+                    return (uf.code === givenUf);
+                });
 
-				if(uf) {
-					scope.lookAt(parseFloat(uf.lat), parseFloat(uf.lng), 6);
-				}
+                if (uf) {
+                    scope.lookAt(parseFloat(uf.lat), parseFloat(uf.lng), 6);
+                }
 
-			};
+            };
 
-			scope.refresh = function() {
-				console.log('[widget.searchable_cases_map] Loading data...');
+            scope.refresh = function () {
+                console.log('[widget.searchable_cases_map] Loading data...');
 
-				Children.getMap({}, function(data) {
-					scope.coordinates = data.coordinates;
-					scope.mapCenter = data.center;
-					scope.mapZoom = data.center.zoom;
-					scope.mapReady = true;
+                Children.getMap({}, function (data) {
+                    scope.coordinates = data.coordinates;
+                    scope.mapCenter = data.center;
+                    scope.mapZoom = data.center.zoom;
+                    scope.mapReady = true;
 
-					console.log("[widget.searchable_cases_map] Data loaded: ", data.coordinates, data.center);
-				});
-			};
+                    console.log("[widget.searchable_cases_map] Data loaded: ", data.coordinates, data.center);
+                });
+            };
 
-			scope.onMarkerClick = function (marker, event, coords) {
-				console.log('[widget.searchable_cases_map] Marker clicked: ', marker, event, coords);
-			};
+            scope.onMarkerClick = function (marker, event, coords) {
+                console.log('[widget.searchable_cases_map] Marker clicked: ', marker, event, coords);
+            };
 
 
+            scope.reloadMap = function () {
+                scope.mapReady = false;
+                $timeout(function () {
+                    scope.mapReady = true;
+                }, 10);
+            };
 
-			scope.reloadMap = function() {
-				scope.mapReady = false;
-				$timeout(function() {
-					scope.mapReady = true;
-				}, 10);
-			};
+            scope.lookAt = function (lat, lng, zoom) {
+                console.log("[widget.searchable_cases_map] Look at @ ", lat, lng, zoom, scope.ctrl);
 
-			scope.lookAt = function(lat, lng, zoom) {
-				console.log("[widget.searchable_cases_map] Look at @ ", lat, lng, zoom, scope.ctrl);
+                scope.ctrl.map.panTo({lat: lat, lng: lng});
+                scope.ctrl.map.setZoom(zoom);
 
-				scope.ctrl.map.panTo({lat: lat, lng: lng});
-				scope.ctrl.map.setZoom(zoom);
+            };
 
-			};
+            scope.isMapReady = function () {
+                return scope.mapReady;
+            };
 
-			scope.showMaps = function () {
-				scope.isMapReady = function() {
-					return scope.mapReady;
-				};
-			}
 
-			uiGmapGoogleMapApi.then(function (maps) {
-				scope.refresh();
-			});
+            uiGmapGoogleMapApi.then(function (maps) {
+                scope.refresh();
+            });
 
-		}
+        }
 
-		return {
-			link: init,
-			replace: true,
-			templateUrl: '/views/components/searchable_cases_map.html'
-		};
-	});
+        return {
+            link: init,
+            replace: true,
+            templateUrl: '/views/components/searchable_cases_map.html'
+        };
+    });
 
 })();
