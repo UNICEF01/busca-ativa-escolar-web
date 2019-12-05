d245f42921b331356bbf26c6b003d7be070206b6
Sandy <ssantos@unicef.org>
2019-12-05 15:13:37 -0200

    Informativo de casos atribuidos ao a usuario a ser excluido

--- resources/assets/js/resources/users.js
+++ resources/assets/js/resources/users.js
@@ -1,23 +1,19 @@
-(function() {
-	angular
-		.module('BuscaAtivaEscolar')
-		.factory('Users', function Users(API, $resource) {
+(function () {
+    angular
+        .module('BuscaAtivaEscolar')
+        .factory('Users', function Users(API, $resource) {
+            var headers = API.REQUIRE_AUTH;
+            var debug = true;
+            var param = debug ? '?XDEBUG_SESSION_START=PHPSTORM' : '';
+            return $resource(API.getURI('users/:id' + param), {id: '@id', with: '@with'}, {
+                myself: {url: API.getURI('users/myself'), method: 'GET', headers: headers},
+                find: {method: 'GET', headers: headers},
+                create: {method: 'POST', headers: headers},
+                update: {method: 'PUT', headers: headers},
+                search: {url: API.getURI('users/search'), method: 'POST', isArray: false, headers: headers},
+                suspend: {method: 'DELETE', headers: headers},
+                restore: {url: API.getURI('users/:id/restore'), method: 'POST', headers: headers},
+            });
 
-			var headers = API.REQUIRE_AUTH;
-
-			var debug = true;
-
-			var param = debug ? '?XDEBUG_SESSION_START=PHPSTORM' : '';
-
-			return $resource(API.getURI('users/:id'+param), {id: '@id', with: '@with'}, {
-				myself: {url: API.getURI('users/myself'), method: 'GET', headers: headers},
-				find: {method: 'GET', headers: headers},
-				create: {method: 'POST', headers: headers},
-				update: {method: 'PUT', headers: headers},
-				search: {url: API.getURI('users/search'), method: 'POST', isArray: false, headers: headers},
-				suspend: {method: 'DELETE', headers: headers},
-				restore: {url: API.getURI('users/:id/restore'), method: 'POST', headers: headers},
-			});
-
-		});
+        });
 })();
