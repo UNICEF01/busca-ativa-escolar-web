1fa025fee2b7203565577c4229de89a46163cbfb
Sandy <ssantos@unicef.org>
2019-12-18 11:57:49 -0200

    Ajuste data em adesoes municipais#404

--- public/js/app.js
+++ public/js/app.js
@@ -2334,11 +2334,11 @@
             var startDate = moment([fDate.getFullYear(), fDate.getMonth(), fDate.getDate()]);
 
             var diffDuration = moment.duration(dateNow.diff(startDate));
-            var year = diffDuration.years() > 0 ? diffDuration.years() + 'A' : '';
-            var month = diffDuration.months() > 0 ? diffDuration.months() + 'M' : '';
-            var days = diffDuration.days() > 0 ? diffDuration.days() + 'D' : '';
+            var year = diffDuration.years() > 0 ? diffDuration.years() + (diffDuration.years() === 1 ? ' ano ' : ' anos ') : '';
+            var month = diffDuration.months() > 0 ? diffDuration.months() + (diffDuration.months() === 1 ? ' mês ' : ' meses ') : '';
+            var days = diffDuration.days() > 0 ? diffDuration.days() + (diffDuration.days() === 1 ? ' dia ' : ' dias') : '';
 
-            return 'há '+year + month + days;
+            return 'Adesão realizada há '+year + month + (days ? 'e ' + days : '');
         };
     });
 })();
@@ -3253,6 +3253,25 @@
 
 })();
 (function() {
+	angular.module('BuscaAtivaEscolar').service('Decorators', function () {
+		var Child = {
+			parents: function(child) {
+				return (child.mother_name || '')
+					+ ((child.mother_name && child.father_name) ? ' | ' : '')
+					+ (child.father_name || '');
+			}
+		};
+
+		var Step = {
+		};
+
+		return {
+			Child: Child,
+			Step: Step
+		};
+	})
+})();
+(function() {
 
 	angular
 		.module('BuscaAtivaEscolar')
@@ -4125,25 +4144,6 @@ Highcharts.maps["countries/br/br-all"] = {
 		}
 	}]
 };
-(function() {
-	angular.module('BuscaAtivaEscolar').service('Decorators', function () {
-		var Child = {
-			parents: function(child) {
-				return (child.mother_name || '')
-					+ ((child.mother_name && child.father_name) ? ' | ' : '')
-					+ (child.father_name || '');
-			}
-		};
-
-		var Step = {
-		};
-
-		return {
-			Child: Child,
-			Step: Step
-		};
-	})
-})();
 (function() {
 	angular
 		.module('BuscaAtivaEscolar')
