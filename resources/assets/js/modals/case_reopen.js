2a4d4c55f8a928d84a61df09473c04e3d6be2c62
Manoel Filho <mansouza@unicef.org>
2020-01-30 18:30:52 -0300

    Mudancas no Modal para Reabertura para funcionar com perfis de coordenacao e supervisao

--- resources/assets/js/modals/case_reopen.js
+++ resources/assets/js/modals/case_reopen.js
@@ -2,12 +2,13 @@
 
 	angular
 		.module('BuscaAtivaEscolar')
-		.controller('CaseReopenModalCtrl', function CaseRestartModalCtrl($scope, $q, $uibModalInstance) {
+		.controller('CaseReopenModalCtrl', function CaseRestartModalCtrl($scope, $q, $uibModalInstance, $typeUser) {
 
 			console.log("[modal] case_restart");
 
 			$scope.step = 1;
 			$scope.reason = "";
+			$scope.typeUser = $typeUser;
 
 			$scope.ok = function() {
 				if(!$scope.reason) return;
