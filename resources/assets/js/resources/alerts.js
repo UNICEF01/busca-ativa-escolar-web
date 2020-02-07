03fcafb6713c8b28473b65417e9b139c161bdb04
Manoel Filho <mansouza@unicef.org>
2020-02-07 10:21:39 -0300

    Correcao compilacao

--- resources/assets/js/resources/alerts.js
+++ resources/assets/js/resources/alerts.js
@@ -10,7 +10,7 @@
 				getPending: {url: API.getURI('alerts/pending'), isArray: false, method: 'GET', headers: headers},
 				mine: {url: API.getURI('alerts/mine'), isArray: false, method: 'GET', headers: headers},
 				accept: {url: API.getURI('alerts/:id/accept'), method: 'POST', headers: headers},
-				reject: {url: API.getURI('alerts/:id/reject'), method: 'POST', headers: headers},
+				reject: {url: API.getURI('alerts/:id/reject'), method: 'POST', headers: headers}
 			});
 		});
 })();
