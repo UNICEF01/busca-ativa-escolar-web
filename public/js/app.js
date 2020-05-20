f3ebc99c5a0440e614836a39baf98cc28ae27e40
Sandy <ssantos@unicef.org>
2020-05-19 21:51:43 -0300

    Ajuste posição do gráfico de evolução de rematrículas#541

--- public/js/app.js
+++ public/js/app.js
@@ -2082,6 +2082,204 @@
 })();
 (function() {
 
+	angular.module('BuscaAtivaEscolar').directive('evolutionGraph', function (moment, Platform, Reports, Report, Charts, Identity, Tenants, StaticData, Config) {
+		
+		function init(scope, element, attrs) {
+
+			var metrics = {};
+
+			scope.static = StaticData;
+
+			scope.options_selo =['TODOS', 'PARTICIPA DO SELO', 'NÃO PARTICIPA DO SELO'];
+
+			scope.query_evolution_graph = {
+				uf: '',
+				tenant_id: '',
+				selo: 'TODOS'
+			};
+
+			scope.schema = [
+				{
+					"name": "Date",
+					"type": "date",
+					"format": "%Y-%m-%d"
+				},
+				{
+					"name": "Rematricula",
+					"type": "string"
+				},
+				{
+					"name": "Unemployment",
+					"type": "number"
+				}
+			];
+
+			scope.dataSource = {
+				caption: {
+					text: "Evolução (Re)Matrículas"
+				},
+				subcaption: {
+					text: "Período de "+moment().subtract(100, "days").format('DD/MM/YYYY')+" até "+moment().format('DD/MM/YYYY')
+				},
+				series: "Rematricula",
+				yaxis: [
+					{
+						format: {
+							formatter: function(obj){
+								var val=null;
+								if( obj.type === "axis")
+								{
+									val= obj.value
+								}
+								else
+								{
+									val= obj.value.toString().replace(".",",");
+								}
+								return val;
+							}
+
+						},
+						plot: [
+							{
+								value: "Unemployment",
+								type: "column"
+							}
+						],
+						title: "(Re)Matrículas",
+						referenceline: [
+							{
+								label: "Meta Selo UNICEF"
+							}
+						],
+						defaultFormat: false
+					}
+				],
+				xAxis: {
+					initialInterval: {
+						from: moment().subtract(100, "days").format('YYYY-MM-DD'),
+						to: moment().format('YYYY-MM-DD')
+					},
+					outputTimeFormat: {
+						year: "%Y",
+						month: "%m/%Y",
+						day: "%d/%m/%Y"
+					},
+					timemarker: [{
+						timeFormat: '%m/%Y'
+					}]
+				},
+				tooltip: {
+					enabled: "false", // Disables the Tooltip
+					outputTimeFormat: {
+						day: "%d/%m/%Y"
+					},
+					style: {
+						container: {
+							"border-color": "#000000",
+							"background-color": "#75748D"
+						},
+						text: {
+							"color": "#FFFFFF"
+						}
+					}
+				}
+			};
+
+			scope.getUFs = function () {
+				return StaticData.getUFs();
+			};
+
+			scope.getData = function () {
+
+				Identity.provideToken().then(function (token) {
+
+					var jsonify = function (res) { return res.json(); }
+
+					var dataDaily = fetch(Config.getAPIEndpoint() + 'reports/data_rematricula_daily?uf='+scope.query_evolution_graph.uf+'&tenant_id='+scope.query_evolution_graph.tenant_id+'&selo='+scope.query_evolution_graph.selo+'&token=' + token).then(jsonify);
+
+					Promise.all([dataDaily]).then( function( res) {
+						const data = res[0];
+
+						var data_final = [
+							{date: moment().format('YYYY-MM-DD'), value: "0", tipo: "(Re)matrícula"},
+							{date: moment().format('YYYY-MM-DD'), value: "0", tipo: "Cancelamento"}
+						];
+
+						if( parseInt(data.data.length) > 0 ) { data_final = data.data; }
+
+						const fusionTable = new FusionCharts.DataStore().createDataTable(
+
+							data_final.map(function(x) {
+								return [
+									x.date,
+									x.tipo,
+									parseFloat(x.value)
+								]
+							}),
+
+							scope.schema
+						);
+						scope.$apply(function () {
+
+							if( data.selo == "PARTICIPA DO SELO" && data.goal > 0) {
+								scope.dataSource.yaxis[0].Max = data.goal;
+								scope.dataSource.yaxis[0].referenceline[0].label = "Meta Selo UNICEF";
+								scope.dataSource.yaxis[0].referenceline[0].value = data.goal;
+							}
+
+							if( data.selo == "NÃO PARTICIPA DO SELO" || data.selo == "TODOS") {
+								scope.dataSource.yaxis[0].Max = 0;
+								scope.dataSource.yaxis[0].referenceline[0] = {};
+							}
+
+							scope.dataSource.data = fusionTable;
+						});
+
+						scope.initTenants();
+					});
+
+				});
+			}
+
+			scope.initTenants = function(){
+				if (Identity.getType() === 'coordenador_estadual') {
+					scope.tenants = Tenants.findByUfPublic({'uf': scope.identity.getCurrentUser().uf});
+				}
+			};
+
+			scope.atualizaDash = function () {
+				scope.tenants = Tenants.findByUfPublic({'uf': scope.query_evolution_graph.uf});
+				scope.getData();
+			};
+
+			scope.onSelectCity = function () {
+				scope.getData();
+			};
+
+			scope.refresh = function () {
+				if ((scope.query.uf !== undefined) && (scope.query.tenant_id !== undefined)) {
+					scope.tenants = Graph.getReinsertEvolution({'uf': scope.query.uf});
+				}
+			};
+
+			scope.getTenants = function () {
+				if (!scope.tenants || !scope.tenants.data) return [];
+				return scope.tenants.data;
+			};
+
+
+		}
+
+		return {
+			link: init,
+			replace: true,
+			templateUrl: '/views/dashboards/grafico_evolucao_rematriculas.html'
+		};
+	});
+
+})();
+(function() {
+
 	angular.module('BuscaAtivaEscolar').directive('appHeaderWarnings', function (Identity, Platform, Auth) {
 
 		function init(scope, element, attrs) {
@@ -7224,126 +7422,6 @@ if (!Array.prototype.find) {
 
 })();
 (function() {
-
-	angular.module('BuscaAtivaEscolar')
-		.config(function ($stateProvider) {
-			$stateProvider.state('school_browser', {
-				url: '/schools',
-				templateUrl: '/views/schools/school_browser.html',
-				controller: 'SchoolBrowserCtrl'
-			});
-		})
-		.controller('SchoolBrowserCtrl', function ($scope, Schools, ngToast, $state, Modals, Identity, Config, Ufs, Platform) {
-                   
-			$scope.check_all_schools = false;
-			$scope.identity = Identity;
-			$scope.schools = {};
-			$scope.msg_success = false;
-			$scope.msg_error = false;
-			$scope.avaliable_years_educacenso = [2017, 2018, 2019];
-			$scope.query = {
-				year_educacenso: 2019,
-				sort: {},
-				show_suspended: false,
-				max: 5,
-				page: 1
-			};
-			$scope.selected = {
-				schools: []
-			};
-			
-			$scope.onCheckSelectAll = function(){
-				if( $scope.check_all_schools ){
-					$scope.selected.schools = angular.copy($scope.schools.data);
-				}else{
-					$scope.selected.schools = [];
-				}
-			};
-
-            $scope.onModifySchool = function(school){
-				Schools.update(school).$promise.then($scope.onSaved);
-			};
-			
-			$scope.onSaved = function(res) {
-				if(res.status === "ok") {
-					ngToast.success("Dados da escola "+res.updated.name+" salvos com sucesso!");
-					return;
-				}else{
-					ngToast.danger("Ocorreu um erro ao salvar a escola!: "+res.message, res.status);
-					$scope.refresh();
-				}
-			};
-
-			$scope.sendnotification = function(){
-
-				//remove objects without email
-				var schools_to_send_notification = $scope.selected.schools.filter(function(school){
-					if(school.school_email != null && school.school_email != ""){
-						return true;
-					}else{
-						return false;
-					}
-				});
-
-				if(schools_to_send_notification.length > 0){
-					
-					Modals.show(
-						Modals.ConfirmEmail(
-							'Confirma o envio de sms e email para as seguintes escolas?',
-							'Ao confirmar, as escolas serão notificadas por email e sms e poderão cadastrar o endereço das crianças e adolescentes reportadas pelo Educacenso',
-							schools_to_send_notification
-						)).then(function () {
-							return Schools.send_notifications(schools_to_send_notification).$promise;
-						})
-						.then(function (res) {
-							if(res.status == "error"){
-								ngToast.danger(res.message);
-								$scope.msg_success = false;
-								$scope.msg_error = true;
-								$scope.refresh();
-								window.scrollTo(0, 0);
-							}else{
-								ngToast.warning(res.message);
-								$scope.msg_success = true;
-								$scope.msg_error = false;
-								$scope.refresh();
-								window.scrollTo(0, 0);
-							}
-						});
-				}else{
-					Modals.show(Modals.Alert('Atenção', 'Selecione as escolas para as quais deseja encaminhar o email/ SMS'));
-				}
-
-			};
-
-			$scope.onSelectYear = function() {
-				$scope.query.page = 1;
-				$scope.query.max = 5;
-				$scope.refresh();
-			};
-
-			$scope.refresh = function() {
-                Schools.all_educacenso($scope.query, function(res) {
-					$scope.check_all_schools = false;
-					$scope.selected.schools = [];
-					$scope.schools = angular.copy(res);
-				});
-			};
-
-			$scope.setMaxResults = function(max) {
-				$scope.query.max = max;
-				$scope.query.page = 1;
-				$scope.refresh();
-			};
-			
-			Platform.whenReady(function() {
-                $scope.refresh();
-			});
-
-		});
-
-})();
-(function() {
 	angular
 		.module('BuscaAtivaEscolar')
 		.factory('Alerts', function Alerts(API, Identity, $resource) {
@@ -7882,6 +7960,126 @@ if (!Array.prototype.find) {
 			});
 
 		});
+})();
+(function() {
+
+	angular.module('BuscaAtivaEscolar')
+		.config(function ($stateProvider) {
+			$stateProvider.state('school_browser', {
+				url: '/schools',
+				templateUrl: '/views/schools/school_browser.html',
+				controller: 'SchoolBrowserCtrl'
+			});
+		})
+		.controller('SchoolBrowserCtrl', function ($scope, Schools, ngToast, $state, Modals, Identity, Config, Ufs, Platform) {
+                   
+			$scope.check_all_schools = false;
+			$scope.identity = Identity;
+			$scope.schools = {};
+			$scope.msg_success = false;
+			$scope.msg_error = false;
+			$scope.avaliable_years_educacenso = [2017, 2018, 2019];
+			$scope.query = {
+				year_educacenso: 2019,
+				sort: {},
+				show_suspended: false,
+				max: 5,
+				page: 1
+			};
+			$scope.selected = {
+				schools: []
+			};
+			
+			$scope.onCheckSelectAll = function(){
+				if( $scope.check_all_schools ){
+					$scope.selected.schools = angular.copy($scope.schools.data);
+				}else{
+					$scope.selected.schools = [];
+				}
+			};
+
+            $scope.onModifySchool = function(school){
+				Schools.update(school).$promise.then($scope.onSaved);
+			};
+			
+			$scope.onSaved = function(res) {
+				if(res.status === "ok") {
+					ngToast.success("Dados da escola "+res.updated.name+" salvos com sucesso!");
+					return;
+				}else{
+					ngToast.danger("Ocorreu um erro ao salvar a escola!: "+res.message, res.status);
+					$scope.refresh();
+				}
+			};
+
+			$scope.sendnotification = function(){
+
+				//remove objects without email
+				var schools_to_send_notification = $scope.selected.schools.filter(function(school){
+					if(school.school_email != null && school.school_email != ""){
+						return true;
+					}else{
+						return false;
+					}
+				});
+
+				if(schools_to_send_notification.length > 0){
+					
+					Modals.show(
+						Modals.ConfirmEmail(
+							'Confirma o envio de sms e email para as seguintes escolas?',
+							'Ao confirmar, as escolas serão notificadas por email e sms e poderão cadastrar o endereço das crianças e adolescentes reportadas pelo Educacenso',
+							schools_to_send_notification
+						)).then(function () {
+							return Schools.send_notifications(schools_to_send_notification).$promise;
+						})
+						.then(function (res) {
+							if(res.status == "error"){
+								ngToast.danger(res.message);
+								$scope.msg_success = false;
+								$scope.msg_error = true;
+								$scope.refresh();
+								window.scrollTo(0, 0);
+							}else{
+								ngToast.warning(res.message);
+								$scope.msg_success = true;
+								$scope.msg_error = false;
+								$scope.refresh();
+								window.scrollTo(0, 0);
+							}
+						});
+				}else{
+					Modals.show(Modals.Alert('Atenção', 'Selecione as escolas para as quais deseja encaminhar o email/ SMS'));
+				}
+
+			};
+
+			$scope.onSelectYear = function() {
+				$scope.query.page = 1;
+				$scope.query.max = 5;
+				$scope.refresh();
+			};
+
+			$scope.refresh = function() {
+                Schools.all_educacenso($scope.query, function(res) {
+					$scope.check_all_schools = false;
+					$scope.selected.schools = [];
+					$scope.schools = angular.copy(res);
+				});
+			};
+
+			$scope.setMaxResults = function(max) {
+				$scope.query.max = max;
+				$scope.query.page = 1;
+				$scope.refresh();
+			};
+			
+			Platform.whenReady(function() {
+                $scope.refresh();
+			});
+
+		});
+
 })();
 (function() {
 	angular
