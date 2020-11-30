39d4c7b5aafb29dcce6ebdac8519d18374849983
Sandy <ssantos@unicef.org>
2020-11-30 11:04:09 -0200

    Padronização das telas de Aceite dos termos de acordo com a LGPD junto com atualização de dados do usuário#724 - ajuste limpeza do storage quando o token for inválido

--- resources/assets/js/tenants/lgpd_signup.js
+++ resources/assets/js/tenants/lgpd_signup.js
@@ -94,7 +94,6 @@
             $scope.getUserTypes = function () {
                 if (!permissions) return {};
                 if (!permissions.can_manage_types) return {};
-                if (!Identity.getCurrentUser()) return {};
 
                 var finalPermissions = permissions.can_manage_types[Identity.getCurrentUser().type].filter(function (el) {
                     return $scope.getUserTypesVisitantes().indexOf(el) < 0;
