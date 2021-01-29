4344d2ff735da7f192dec82d7be9205d10b46805
Manoel Souza <mansouza@unicef.org>
2021-01-29 09:46:17 -0300

    Exibicao dos casos na tela relatorios para aqueles municipios que ja aderiram em algum momento

--- resources/assets/js/resources/reports.js
+++ resources/assets/js/resources/reports.js
@@ -6,7 +6,7 @@
 			var headers = API.REQUIRE_AUTH;
 
 			return $resource(API.getURI('reports/:entity'), {entity: '@entity'}, {
-				query: {url: API.getURI('reports/:entity'), method: 'POST', headers: headers},
+				query: {url: API.getURI('reports/:entity?XDEBUG_SESSION_START=PHPSTORM'), method: 'POST', headers: headers},
 				getCountryStats: {method: 'GET', url: API.getURI('reports/country_stats'), headers: headers},
 				getStateStats: {method: 'GET', url: API.getURI('reports/state_stats'), headers: headers},
 				getStatusBar: {method: 'GET', url: API.getURI('reports/city_bar'), headers: headers},
