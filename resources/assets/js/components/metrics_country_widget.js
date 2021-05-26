bdb743b446ad0c0d799e39f34c4e2f744532413c
rafaelecs1 <74567820+rafaelecs1@users.noreply.github.com>
2021-05-26 09:46:43 -0300

    Subindo os dados (estáticos) e as opções para seleção no painel para os casos e alertas, ano a ano.

--- resources/assets/js/components/metrics_country_widget.js
+++ resources/assets/js/components/metrics_country_widget.js
@@ -1,59 +1,129 @@
-(function() {
+(function () {
+  angular
+    .module('BuscaAtivaEscolar')
+    .directive('metricsCountry', function (moment, Platform, Reports, Charts) {
+      function init(scope, element, attrs) {
+        scope.availableOptions = [
+          { id: 1, name: 'Ciclo Estratégia | 2021 - 2024 (Status atual)' },
+          { id: 2, name: 'Ciclo Estratégia 2014-2020 (Status em 31/12/2020)' },
+          { id: 3, name: 'Ciclo Estratégia 2014-2020 (Status em 31/12/2019)' },
+          { id: 4, name: 'Ciclo Estratégia 2014-2020 (Status em 31/12/2018)' },
+          { id: 5, name: 'Ciclo Estratégia 2014-2020 (Status em 31/12/2017)' }
+        ];
+        scope.selectedOption = {
+          id: 1,
+          name: 'Ciclo Estratégia | 2021 - 2024 (Status atual)'
+        };
 
-	angular.module('BuscaAtivaEscolar').directive('metricsCountry', function (moment, Platform, Reports, Charts) {
+        scope.stats = {};
 
-		function init(scope, element, attrs) {
+        scope.stats_ciclo_2 = {
+          num_tenants: 2518,
+          num_ufs: 21,
+          num_signups: 3214,
+          num_pending_setup: 695,
+          num_alerts: 160719,
+          num_pending_alerts: 110836,
+          num_rejected_alerts: 79865,
+          num_total_alerts: 351420,
+          num_cases_in_progress: 121644,
+          num_children_reinserted: 79595,
+          num_pending_signups: 1,
+          num_pending_state_signups: 1,
+          num_children_in_school: 6050,
+          num_children_in_observation: 73545,
+          num_children_out_of_school: 48060,
+          num_children_cancelled: 32299,
+          num_children_transferred: 188,
+          num_children_interrupted: 577
+        };
 
-			scope.availableOptions = [
-				{id: 1, name: "Ciclo Selo | 2014 - 2020"},
-				{id: 2, name: "Ciclo Selo | 2021"},
-			];
-			scope.selectedOption = {id: 2, name: "Ciclo Selo | 2021"};
+        scope.stats_ciclo_3 = {
+          num_tenants: 2518,
+          num_ufs: 21,
+          num_signups: 3214,
+          num_pending_setup: 695,
+          num_alerts: 55106,
+          num_pending_alerts: 88518,
+          num_rejected_alerts: 42081,
+          num_total_alerts: 185705,
+          num_cases_in_progress: 121644,
+          num_children_reinserted: 79595,
+          num_pending_signups: 1,
+          num_pending_state_signups: 1,
+          num_children_in_school: 307,
+          num_children_in_observation: 16800,
+          num_children_out_of_school: 26040,
+          num_children_cancelled: 11959,
+          num_children_transferred: 0,
+          num_children_interrupted: 0
+        };
+        scope.stats_ciclo_4 = {
+          num_tenants: 2518,
+          num_ufs: 21,
+          num_signups: 3214,
+          num_pending_setup: 695,
+          num_alerts: 5776,
+          num_pending_alerts: 24866,
+          num_rejected_alerts: 11663,
+          num_total_alerts: 42305,
+          num_cases_in_progress: 121644,
+          num_children_reinserted: 79595,
+          num_pending_signups: 1,
+          num_pending_state_signups: 1,
+          num_children_in_school: 143,
+          num_children_in_observation: 316,
+          num_children_out_of_school: 4655,
+          num_children_cancelled: 662,
+          num_children_transferred: 0,
+          num_children_interrupted: 0
+        };
 
-			scope.stats = {};
+        scope.stats_ciclo_5 = {
+          num_tenants: 2518,
+          num_ufs: 21,
+          num_signups: 3214,
+          num_pending_setup: 695,
+          num_alerts: 114,
+          num_pending_alerts: 149,
+          num_rejected_alerts: 10,
+          num_total_alerts: 273,
+          num_cases_in_progress: 121644,
+          num_children_reinserted: 79595,
+          num_pending_signups: 1,
+          num_pending_state_signups: 1,
+          num_children_in_school: 2,
+          num_children_in_observation: 1,
+          num_children_out_of_school: 102,
+          num_children_cancelled: 9,
+          num_children_transferred: 0,
+          num_children_interrupted: 0
+        };
 
-			scope.stats_ciclo_1 = {
-				num_tenants: 2518,
-				num_ufs: 21,
-				num_signups: 3214,
-				num_pending_setup: 695,
-				num_alerts: 160712,
-				num_pending_alerts: 110900,
-				num_rejected_alerts: 79865,
-				num_total_alerts: 351542,
-				num_cases_in_progress: 121644,
-				num_children_reinserted: 79595,
-				num_pending_signups: 1,
-				num_pending_state_signups: 1,
-				num_children_in_school: 6069,
-				num_children_in_observation: 73526,
-				num_children_out_of_school: 48118,
-				num_children_cancelled: 32299,
-				num_children_transferred: 188,
-				num_children_interrupted: 577
-			};
+        function refreshMetrics() {
+          return Reports.getCountryStats(function (data) {
+            if (data.status !== 'ok') {
+              ngToast.danger(
+                'Ocorreu um erro ao carregar os números gerais da plataforma. (err: ' +
+                  data.reason +
+                  ')'
+              );
+              return;
+            }
 
-			function refreshMetrics() {
-				return Reports.getCountryStats(function (data) {
-					if(data.status !== 'ok') {
-						ngToast.danger('Ocorreu um erro ao carregar os números gerais da plataforma. (err: ' + data.reason + ')');
-						return;
-					}
+            scope.stats = data.stats;
+          });
+        }
 
-					scope.stats = data.stats;
-				});
-			}
+        Platform.whenReady(function () {
+          refreshMetrics();
+        });
+      }
 
-			Platform.whenReady(function () {
-				refreshMetrics();
-			});
-		}
-
-		return {
-			link: init,
-			replace: true,
-			templateUrl: '/views/components/metrics_country.html'
-		};
-	});
-
+      return {
+        link: init,
+        replace: true,
+        templateUrl: '/views/components/metrics_country.html'
+      };
+    });
 })();
