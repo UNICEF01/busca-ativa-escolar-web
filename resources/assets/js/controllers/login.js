818f4d12b5d06778bc60ae26a4b3b2eeec5d8829
rafaelecs1 <74567820+rafaelecs1@users.noreply.github.com>
2021-08-02 20:05:45 -0300

    Subindo o popup depois de logar

--- resources/assets/js/controllers/login.js
+++ resources/assets/js/controllers/login.js
@@ -1,73 +1,92 @@
 (function () {
+  angular
+    .module('BuscaAtivaEscolar')
+    .controller(
+      'LoginCtrl',
+      function (
+        $scope,
+        $rootScope,
+        $cookies,
+        $state,
+        $location,
+        Modals,
+        Config,
+        Auth,
+        Identity
+      ) {
+        //console.log("[core] @Login");
 
-  angular.module('BuscaAtivaEscolar').controller('LoginCtrl', function ($scope, $rootScope, $cookies, $state, $location, Modals, Config, Auth, Identity) {
+        $rootScope.section = '';
 
-    //console.log("[core] @Login");
+        $scope.email = '';
+        $scope.password = '';
+        $scope.isLoading = false;
 
-    $rootScope.section = '';
+        $scope.endpoints = {
+          allowed: Config.ALLOWED_ENDPOINTS,
+          list: Config.API_ENDPOINTS,
+        };
 
-    $scope.email = '';
-    $scope.password = '';
-    $scope.isLoading = false;
+        if (Identity.isLoggedIn()) {
+          $state.go('dashboard');
+        }
 
-    $scope.endpoints = {
-      allowed: Config.ALLOWED_ENDPOINTS,
-      list: Config.API_ENDPOINTS
-    };
+        $scope.showPassowrd = function () {
+          var field_password = document.getElementById('fld-password');
+          field_password.type === 'password'
+            ? (field_password.type = 'text')
+            : (field_password.type = 'password');
+        };
 
-    if (Identity.isLoggedIn()) {
-      $state.go('dashboard');
-    }
+        function onLoggedIn(session) {
+          $scope.isLoading = false;
+          Modals.show(
+            Modals.GeneralPopUpAlerts(
+              'QUESTIONÁRIO SOBRE IMPLEMENTAÇÃO DA ESTRATÉGIA'
+            )
+          );
 
-    $scope.showPassowrd = function () {
-      var field_password = document.getElementById("fld-password");
-      field_password.type === "password" ? field_password.type = "text" : field_password.type = "password"
-    }
+          if (Identity.getCurrentUser().lgpd) {
+            if (!Identity.isUserType('coordenador_operacional'))
+              return $state.go('dashboard');
+            if (!Identity.hasTenant()) return $state.go('dashboard');
+            if (!Identity.getCurrentUser().tenant.is_setup)
+              return $state.go('tenant_setup');
 
-    function onLoggedIn(session) {
-      $scope.isLoading = false;
+            return $state.go('dashboard');
+          } else {
+            return $state.go('lgpd_signup');
+          }
+        }
 
-      //console.info("[login_ctrl] Logged in!", session);
-      //console.info("[login_ctrl] Tenant: ", Identity.getCurrentUser().tenant);
+        function onError(err) {
+          console.error('[login_ctrl] Login failed: ', err);
+          Modals.show(
+            Modals.Alert(
+              'Usuário ou senha incorretos',
+              'Por favor, verifique os dados informados e tente novamente.'
+            )
+          );
+          $scope.isLoading = false;
+        }
 
-      if (Identity.getCurrentUser().lgpd) {
-        if (!Identity.isUserType('coordenador_operacional')) return $state.go('dashboard');
-        if (!Identity.hasTenant()) return $state.go('dashboard');
-        if (!Identity.getCurrentUser().tenant.is_setup) return $state.go('tenant_setup');
+        $scope.setAPIEndpoint = function (endpointID) {
+          Config.setEndpoint(endpointID);
+          $cookies.put('FDENP_API_ENDPOINT', endpointID);
+        };
 
-        return $state.go('dashboard');
+        $scope.login = function () {
+          $scope.isLoading = true;
+          Auth.login($scope.email, $scope.password).then(onLoggedIn, onError);
+        };
 
-      } else {
-        return $state.go('lgpd_signup');
+        $scope.showModalAdesao = function () {
+          Modals.show(
+            Modals.GeneralAlerts('Atenção: Renovação de adesão dos municípios')
+          );
+        };
+
+        $scope.showModalAdesao();
       }
-
-
-      // Check if user should see tenant first time setup
-
-    }
-
-    function onError(err) {
-      console.error('[login_ctrl] Login failed: ', err);
-      Modals.show(Modals.Alert('Usuário ou senha incorretos', 'Por favor, verifique os dados informados e tente novamente.'));
-      $scope.isLoading = false;
-    }
-
-    $scope.setAPIEndpoint = function (endpointID) {
-      Config.setEndpoint(endpointID);
-      $cookies.put('FDENP_API_ENDPOINT', endpointID);
-    };
-
-    $scope.login = function () {
-      $scope.isLoading = true;
-      Auth.login($scope.email, $scope.password).then(onLoggedIn, onError);
-    };
-
-    $scope.showModalAdesao = function () {
-      Modals.show(Modals.GeneralAlerts('Atenção: Renovação de adesão dos municípios'));
-    };
-
-    $scope.showModalAdesao();
-
-  });
-
+    );
 })();
