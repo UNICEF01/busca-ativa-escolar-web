9679d324ec53f644ffdb3c385df9478024f6c37b
rafaelecs1 <74567820+rafaelecs1@users.noreply.github.com>
2021-08-05 14:03:07 -0300

    Subindo o campo nacionalidade da criança para criacao do alerta

--- resources/assets/js/resources/static_data.js
+++ resources/assets/js/resources/static_data.js
@@ -1,131 +1,198 @@
-(function() {
-	angular
-		.module('BuscaAtivaEscolar')
-		.factory('StaticData', function StaticData(API, Identity, $rootScope, $http) {
+(function () {
+  angular
+    .module('BuscaAtivaEscolar')
+    .factory(
+      'StaticData',
+      function StaticData(API, Identity, $rootScope, $http) {
+        var data = {};
+        var self = this;
 
-			var data = {};
-			var self = this;
+        var dataFile = API.getURI('static/static_data?version=latest');
+        var $promise = {};
 
-			var dataFile = API.getURI('static/static_data?version=latest');
-			var $promise = {};
+        // TODO: cache this?
 
-			// TODO: cache this?
+        function fetchLatestVersion() {
+          //console.log("[platform.static_data] Downloading latest static data definitions...");
+          $promise = $http.get(dataFile).then(onFetch);
+        }
 
-			function fetchLatestVersion() {
-				//console.log("[platform.static_data] Downloading latest static data definitions...");
-				$promise = $http.get(dataFile).then(onFetch);
-			}
+        function refresh() {
+          // TODO: validate timestamp?
+          fetchLatestVersion();
+        }
 
-			function refresh() {
-				// TODO: validate timestamp?
-				fetchLatestVersion();
-			}
+        function onFetch(res) {
+          //console.log("[platform.static_data] Downloaded! Version=", res.data.version, "Timestamp=", res.data.timestamp, "Data=", res.data.data);
+          data = res.data.data;
 
-			function onFetch(res) {
-				//console.log("[platform.static_data] Downloaded! Version=", res.data.version, "Timestamp=", res.data.timestamp, "Data=", res.data.data);
-				data = res.data.data;
+          $rootScope.$broadcast('StaticData.ready');
+        }
 
-				$rootScope.$broadcast('StaticData.ready');
-			}
+        function getDataFile() {
+          return dataFile;
+        }
 
-			function getDataFile() {
-				return dataFile;
-			}
+        function getNumChains() {
+          return data.length ? data.length : 0;
+        }
 
-			function getNumChains() {
-				return data.length ? data.length : 0;
-			}
+        function isReady() {
+          return getNumChains() > 0;
+        }
+        // Ordena pelo valor do indice do objeto
+        function orderMotives(value) {
+          return _.orderBy(value, ['label'], ['asc']);
+        }
 
-			function isReady() {
-				return getNumChains() > 0;
-			}
-			// Ordena pelo valor do indice do objeto
-			function orderMotives(value) {
-				return _.orderBy(value, ['label'], ['asc']);
-			}
+        function getUserTypes() {
+          return data.UserType ? data.UserType : [];
+        }
 
-			function getUserTypes() { return (data.UserType) ? data.UserType : []; }
-			function getAlertCauses() {	return (data.AlertCause) ? orderMotives(data.AlertCause) : [];}
-			function getVisibleAlertCauses() { return (data.VisibleAlertCause) ? orderMotives(data.VisibleAlertCause) : [];}
-			function getCaseCauses() { return (data.CaseCause) ? data.CaseCause : []; }
-			function getVisibleCaseCauses() { return (data.VisibleCaseCause) ? orderMotives(data.VisibleCaseCause) : []; }
-			function getGenders() { return (data.Gender) ? data.Gender : []; }
-			function getHandicappedRejectReasons() { return (data.HandicappedRejectReason) ? data.HandicappedRejectReason : []; }
-			function getAgeRanges() { return (data.AgeRange) ? data.AgeRange : []; }
-			function getIncomeRanges() { return (data.IncomeRange) ? data.IncomeRange : []; }
-			function getRaces() { return (data.Race) ? data.Race : []; }
-			function getSchoolGrades() { return (data.SchoolGrade) ? data.SchoolGrade : []; }
-			function getSchoolingLevels() { return (data.SchoolingLevel) ? data.SchoolingLevel : []; }
-			function getWorkActivities() { return (data.WorkActivity) ? data.WorkActivity : []; }
-			function getCaseStepSlugs() { return (data.CaseStepSlugs) ? data.CaseStepSlugs : []; }
-			function getUFs() { return (data.UFs) ? data.UFs : []; }
-			function getUFsDropdown() {
-				var dropdown = [];
+        function getAlertCauses() {
+          return data.AlertCause ? orderMotives(data.AlertCause) : [];
+        }
+        function getVisibleAlertCauses() {
+          return data.VisibleAlertCause
+            ? orderMotives(data.VisibleAlertCause)
+            : [];
+        }
+        function getCaseCauses() {
+          return data.CaseCause ? data.CaseCause : [];
+        }
+        function getVisibleCaseCauses() {
+          return data.VisibleCaseCause
+            ? orderMotives(data.VisibleCaseCause)
+            : [];
+        }
+        function getGenders() {
+          return data.Gender ? data.Gender : [];
+        }
+        function getHandicappedRejectReasons() {
+          return data.HandicappedRejectReason
+            ? data.HandicappedRejectReason
+            : [];
+        }
+        function getAgeRanges() {
+          return data.AgeRange ? data.AgeRange : [];
+        }
+        function getIncomeRanges() {
+          return data.IncomeRange ? data.IncomeRange : [];
+        }
+        function getRaces() {
+          return data.Race ? data.Race : [];
+        }
 
-				angular.forEach(data.UFsByCode, function (uf, key) {
-					dropdown.push(uf);
-				});
+        function getChildrenCountry() {
+          return data.ChildrenCountry ? data.ChildrenCountry : [];
+        }
+        function getSchoolGrades() {
+          return data.SchoolGrade ? data.SchoolGrade : [];
+        }
+        function getSchoolingLevels() {
+          return data.SchoolingLevel ? data.SchoolingLevel : [];
+        }
+        function getWorkActivities() {
+          return data.WorkActivity ? data.WorkActivity : [];
+        }
+        function getCaseStepSlugs() {
+          return data.CaseStepSlugs ? data.CaseStepSlugs : [];
+        }
+        function getUFs() {
+          return data.UFs ? data.UFs : [];
+        }
+        function getUFsDropdown() {
+          var dropdown = [];
 
-				return dropdown;
-			}
-			function getUFByCode(code) { return (data.UFsByCode) ? data.UFsByCode[code] : null; }
-			function getRegions() { return (data.Regions) ? data.Regions : []; }
-			function getTypesWithGlobalScope() { return (data.UsersWithGlobalScope) ? data.UsersWithGlobalScope : []; }
-			function getTypesWithUFScope() { return (data.UsersWithUFScope) ? data.UsersWithUFScope : []; }
-			function getAPIEndpoints() { return (data.APIEndpoints) ? data.APIEndpoints : []; }
-			function getCaseCancelReasons() { return (data.CaseCancelReasons) ? data.CaseCancelReasons : []; }
-			function getAllowedMimeTypes() { return (data.Config) ? data.Config.uploads.allowed_mime_types: ['image/jpeg', 'image/png']; }
-			function getPermissions() { return (data.Permissions) ? data.Permissions : {}; }
+          angular.forEach(data.UFsByCode, function (uf, key) {
+            dropdown.push(uf);
+          });
 
-			function getUserTypeVisitantes() { return (data.UserTypeVisitantes) ? data.UserTypeVisitantes : []; }
-			
-			function getPermissionsFormForVisitante() { return (data.PermissionsFormForVisitante) ? data.PermissionsFormForVisitante : []; }
+          return dropdown;
+        }
+        function getUFByCode(code) {
+          return data.UFsByCode ? data.UFsByCode[code] : null;
+        }
+        function getRegions() {
+          return data.Regions ? data.Regions : [];
+        }
+        function getTypesWithGlobalScope() {
+          return data.UsersWithGlobalScope ? data.UsersWithGlobalScope : [];
+        }
+        function getTypesWithUFScope() {
+          return data.UsersWithUFScope ? data.UsersWithUFScope : [];
+        }
+        function getAPIEndpoints() {
+          return data.APIEndpoints ? data.APIEndpoints : [];
+        }
+        function getCaseCancelReasons() {
+          return data.CaseCancelReasons ? data.CaseCancelReasons : [];
+        }
+        function getAllowedMimeTypes() {
+          return data.Config
+            ? data.Config.uploads.allowed_mime_types
+            : ['image/jpeg', 'image/png'];
+        }
+        function getPermissions() {
+          return data.Permissions ? data.Permissions : {};
+        }
 
-			function getCurrentUF() {
-				var user = Identity.getCurrentUser();
-				if(!user) return null;
-				if(!user.uf) return null;
+        function getUserTypeVisitantes() {
+          return data.UserTypeVisitantes ? data.UserTypeVisitantes : [];
+        }
 
-				return getUFByCode(user.uf);
-			}
+        function getPermissionsFormForVisitante() {
+          return data.PermissionsFormForVisitante
+            ? data.PermissionsFormForVisitante
+            : [];
+        }
 
-			return {
-				fetchLatestVersion: fetchLatestVersion,
-				refresh: refresh,
-				getUserTypes: getUserTypes,
-				getAlertCauses: getAlertCauses,
-				getVisibleAlertCauses: getVisibleAlertCauses,
-				getCaseCauses: getCaseCauses,
-				getVisibleCaseCauses: getVisibleCaseCauses,
-				getGenders: getGenders,
-				getHandicappedRejectReasons: getHandicappedRejectReasons,
-				getIncomeRanges: getIncomeRanges,
-				getAgeRanges: getAgeRanges,
-				getRaces: getRaces,
-				getSchoolGrades: getSchoolGrades,
-				getSchoolingLevels: getSchoolingLevels,
-				getWorkActivities: getWorkActivities,
-				getCaseStepSlugs: getCaseStepSlugs,
-				getAllowedMimeTypes: getAllowedMimeTypes,
-				getUFs: getUFs,
-				getUFsDropdown: getUFsDropdown,
-				getUFByCode: getUFByCode,
-				getCurrentUF: getCurrentUF,
-				getRegions: getRegions,
-				getTypesWithGlobalScope: getTypesWithGlobalScope,
-				getTypesWithUFScope: getTypesWithUFScope,
-				getAPIEndpoints: getAPIEndpoints,
-				getCaseCancelReasons: getCaseCancelReasons,
-				isReady: isReady,
-				getNumChains: getNumChains,
-				getDataFile: getDataFile,
-				getPermissions: getPermissions,
-				getUserTypeVisitantes: getUserTypeVisitantes,
-				getPermissionsFormForVisitante: getPermissionsFormForVisitante
-			};
+        function getCurrentUF() {
+          var user = Identity.getCurrentUser();
+          if (!user) return null;
+          if (!user.uf) return null;
 
-		})
-		.run(function (StaticData) {
-			StaticData.refresh();
-		});
+          return getUFByCode(user.uf);
+        }
+
+        return {
+          fetchLatestVersion: fetchLatestVersion,
+          refresh: refresh,
+          getUserTypes: getUserTypes,
+          getAlertCauses: getAlertCauses,
+          getVisibleAlertCauses: getVisibleAlertCauses,
+          getCaseCauses: getCaseCauses,
+          getVisibleCaseCauses: getVisibleCaseCauses,
+          getGenders: getGenders,
+          getHandicappedRejectReasons: getHandicappedRejectReasons,
+          getIncomeRanges: getIncomeRanges,
+          getAgeRanges: getAgeRanges,
+          getRaces: getRaces,
+          getChildrenCountry: getChildrenCountry,
+          getSchoolGrades: getSchoolGrades,
+          getSchoolingLevels: getSchoolingLevels,
+          getWorkActivities: getWorkActivities,
+          getCaseStepSlugs: getCaseStepSlugs,
+          getAllowedMimeTypes: getAllowedMimeTypes,
+          getUFs: getUFs,
+          getUFsDropdown: getUFsDropdown,
+          getUFByCode: getUFByCode,
+          getCurrentUF: getCurrentUF,
+          getRegions: getRegions,
+          getTypesWithGlobalScope: getTypesWithGlobalScope,
+          getTypesWithUFScope: getTypesWithUFScope,
+          getAPIEndpoints: getAPIEndpoints,
+          getCaseCancelReasons: getCaseCancelReasons,
+          isReady: isReady,
+          getNumChains: getNumChains,
+          getDataFile: getDataFile,
+          getPermissions: getPermissions,
+          getUserTypeVisitantes: getUserTypeVisitantes,
+          getPermissionsFormForVisitante: getPermissionsFormForVisitante,
+        };
+      }
+    )
+    .run(function (StaticData) {
+      StaticData.refresh();
+    });
 })();
