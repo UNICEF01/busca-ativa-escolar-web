a6898265268c3a9dbcb1f480836d045282db10d2
Rafael Emerick <rcorreia@unicef.org>
2021-10-18 17:12:04 +0000

    subindo versao para limpar cache

--- gulpfile.js
+++ gulpfile.js
@@ -3,34 +3,34 @@ require('laravel-elixir-sass-compass');
 require('laravel-elixir-wiredep');
 require('laravel-elixir-bower');
 
-elixir(function(mix) {
+elixir(function (mix) {
+  var defaultCompassSettings = {
+    modules: ['sass-css-importer'],
+    config_file: 'config.rb',
+    style: 'expanded',
+    sass: 'resources/assets/sass',
+    font: 'public/fonts',
+    image: 'public/images',
+    javascript: 'public/js',
+    sourcemap: true,
+  };
+  mix.version('public/css/app.scss');
+  mix.version('public/css/vendor.scss');
+  mix.compass('vendor.scss', 'public/css', defaultCompassSettings);
+  mix.compass('app.scss', 'public/css', defaultCompassSettings);
 
-	var defaultCompassSettings = {
-		modules: ['sass-css-importer'],
-		config_file: "config.rb",
-		style: "expanded",
-		sass: "resources/assets/sass",
-		font: "public/fonts",
-		image: "public/images",
-		javascript: "public/js",
-		sourcemap: true
-	};
+  var dependencies = require('wiredep')().js;
+  var desiredDependencies = [];
 
-	mix.compass('vendor.scss', 'public/css', defaultCompassSettings);
-	mix.compass('app.scss', 'public/css', defaultCompassSettings);
+  for (var i in dependencies) {
+    if (!dependencies.hasOwnProperty(i)) continue;
+    if (dependencies[i].indexOf('tinymce-dist') !== -1) continue;
 
-	var dependencies = require('wiredep')().js;
-	var desiredDependencies = [];
+    desiredDependencies.push(dependencies[i]);
+  }
 
-	for(var i in dependencies) {
-		if(!dependencies.hasOwnProperty(i)) continue;
-		if(dependencies[i].indexOf('tinymce-dist') !== -1) continue;
-
-		desiredDependencies.push(dependencies[i]);
-	}
-
-	mix.scripts(desiredDependencies, 'public/js/vendor.js', '/');
-	mix.scriptsIn('resources/assets/js*', 'public/js/app.js');
-	mix.browserify('app.js');
-
+  mix.scripts(desiredDependencies, 'public/js/vendor.js', '/');
+  mix.scriptsIn('resources/assets/js*', 'public/js/app.js');
+  mix.version('app.js');
+  mix.browserify('app.js');
 });
