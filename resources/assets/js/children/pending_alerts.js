d71c3c599941a9f5e20d5fdd232ddee8e53a0365
Rafael Emerick <rcorreia@unicef.org>
2022-04-13 12:53:02 -0300

    adicionando filtro de grupo alertas

--- resources/assets/js/children/pending_alerts.js
+++ resources/assets/js/children/pending_alerts.js
@@ -1,106 +1,77 @@
 (function() {
 
-	angular.module('BuscaAtivaEscolar')
-		.config(function ($stateProvider) {
-			$stateProvider.state('pending_alerts', {
-				url: '/pending_alerts',
-				templateUrl: '/views/children/pending_alerts.html',
-				controller: 'PendingAlertsCtrlCtrl'
-			})
-		})
-		.controller('PendingAlertsCtrlCtrl', function ($scope, Groups, Platform, Identity, Alerts, StaticData) {
+    angular.module('BuscaAtivaEscolar')
+        .config(function($stateProvider) {
+            $stateProvider.state('pending_alerts', {
+                url: '/pending_alerts',
+                templateUrl: '/views/children/pending_alerts.html',
+                controller: 'PendingAlertsCtrlCtrl'
+            })
+        })
+        .controller('PendingAlertsCtrlCtrl', function($scope, Groups, Platform, Identity, Alerts, StaticData, Modals) {
 
-			$scope.identity = Identity;
-			$scope.sendingAlert = false;
-			$scope.children = {};
-			$scope.child = {};
-			$scope.causes = {};
-			$scope.causes_filter = [];
+            $scope.identity = Identity;
+            $scope.sendingAlert = false;
+            $scope.children = {};
+            $scope.child = {};
+            $scope.causes = {};
+            $scope.causes_filter = [];
 
-			$scope.query = {
+            $scope.query = {
                 name: null,
-				submitter_name: null,
+                submitter_name: null,
                 sort: {},
                 max: 16,
-				page: 1,
-				neighborhood: null,
-				show_suspended: false,
-				group_id: null,
+                page: 1,
+                neighborhood: null,
+                show_suspended: false,
+                group_id: null,
             };
 
             $scope.search = {};
 
-			$scope.groups =  [];
-            if($scope.groups.length == 0){
-                Groups.findUserGroups(function(res){
-                    res.data.forEach(function(v){
-                        $scope.groups.push(({value: v.id, displayName: v.name}));
-                        const size = Object.keys(v).length;
-                        if(size > 2){
-                            for(let i  = 0; i < size - 2; ++i){
-                                v[i].name = v[i].name.trim()
-                                v[i].name = Array(6).fill('\xa0').join('') + v[i].name
-                                $scope.groups.push(({value: v[i].id, displayName: v[i].name}));
-                                const size1 = Object.keys(v[i]).length;
-                                if(size1 > 2){
-                                    for(let j  = 0; j < size1 - 2; ++j){
-                                        v[i][j].name = v[i][j].name.trim()
-                                        v[i][j].name = Array(12).fill('\xa0').join('') + v[i][j].name
-                                        $scope.groups.push(({value: v[i][j].id, displayName: v[i][j].name}));
-                                        const size2 = Object.keys(v[i][j]).length;
-                                        if(size2 > 2){
-                                            for(let l  = 0; l < size2 - 2; ++l){
-                                                v[i][j][l].name = v[i][j][l].name.trim()
-                                                v[i][j][l].name = Array(18).fill('\xa0').join('') + v[i][j][l].name
-                                                $scope.groups.push(({value: v[i][j][l].id, displayName: v[i][j][l].name}));
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    });
+            $scope.changeGroup = function() {
+                Modals.show(
+                    Modals.GroupPicker(
+                        'Atribuir grupo ao alerta',
+                        'O último grupo selecionado será atrinuído ao alerta:',
+                        true)
+                ).then(function(selectedGroup) {
+                    $scope.selectedGroup = selectedGroup;
+                }).then(function(res) {
+
                 });
-            }
+            };
 
-			$(function() {
-				$('#select_group_alert').bind("change", function() {
-					var space_offset =7;
-					var matches = $('#select_group_alert option:selected').text().search(/\S/);
-					var number = matches == 23 ? 23: matches == 29 ? 50 : matches == 35 ? 78: '';
-					$(this).css('text-indent', -(number));
-				});
-			});
+            $scope.branchGroups = "carregando ...";
 
-			$scope.branchGroups = "carregando ...";
-
-			$scope.clikcInGroup = function (group_id){
+            $scope.clikcInGroup = function(group_id) {
                 $scope.branchGroups = "carregando ...";
-                Groups.findByIdWithParents({id: group_id}, function (res){
+                Groups.findByIdWithParents({ id: group_id }, function(res) {
                     var groupOfuserWithParents = res.data[0];
                     var groupsOfUser = [];
                     groupsOfUser.push(groupOfuserWithParents.name);
-                    if ( groupOfuserWithParents.parent != null) {
+                    if (groupOfuserWithParents.parent != null) {
                         groupsOfUser.push(groupOfuserWithParents.parent.name);
-                        if ( groupOfuserWithParents.parent.parent != null) {
+                        if (groupOfuserWithParents.parent.parent != null) {
                             groupsOfUser.push(groupOfuserWithParents.parent.parent.name);
-                            if ( groupOfuserWithParents.parent.parent.parent != null) {
+                            if (groupOfuserWithParents.parent.parent.parent != null) {
                                 groupsOfUser.push(groupOfuserWithParents.parent.parent.parent.name);
                             }
                         }
                     }
                     $scope.branchGroups = groupsOfUser.reverse().join(' > ');
                 });
-			}
-			
-			$scope.getAlertCauseName = function(id) {
-				if(!$scope.child) return 'err:no_child_open';
-				if(!$scope.child.alert) return 'err:no_alert_data';
-				if(!$scope.child.alert.alert_cause_id) return 'err:no_alert_cause_id';
-				var indexAlertCauses = _.findIndex($scope.causes, {id: $scope.child.alert.alert_cause_id});
-				if(!$scope.causes[indexAlertCauses]) return 'err:no_cause_with_id';
-				return $scope.causes[indexAlertCauses].label;
-			};
+            }
+
+            $scope.getAlertCauseName = function(id) {
+                if (!$scope.child) return 'err:no_child_open';
+                if (!$scope.child.alert) return 'err:no_alert_data';
+                if (!$scope.child.alert.alert_cause_id) return 'err:no_alert_cause_id';
+                var indexAlertCauses = _.findIndex($scope.causes, { id: $scope.child.alert.alert_cause_id });
+                if (!$scope.causes[indexAlertCauses]) return 'err:no_cause_with_id';
+                return $scope.causes[indexAlertCauses].label;
+            };
 
             $scope.setMaxResults = function(max) {
                 $scope.query.max = max;
@@ -108,74 +79,75 @@
                 $scope.refresh();
             };
 
-			$scope.static = StaticData;
-			
-			$scope.refresh = function() {
-				$scope.child = null;
-				$scope.children = Alerts.getPending($scope.query);
-				$scope.search = $scope.children;
-			};
+            $scope.static = StaticData;
 
-			$scope.preview = function(child) {
-				$scope.child = child
-				$('#modalChild').modal({
-					keyboard: false,
-				});
-			};
+            $scope.refresh = function() {
+                $scope.child = null;
+                $scope.children = Alerts.getPending($scope.query);
+                $scope.search = $scope.children;
+            };
 
-			$scope.canAcceptAlert = function(child) {
-				if(!child) return false;
-				if(!child.requires_address_update) return true;
-				return child.alert
-					&& child.alert.place_address
-					&& (child.alert.place_address.trim().length > 0)
-					&& child.alert.place_neighborhood
-					&& (child.alert.place_neighborhood.trim().length > 0);
-			};
+            $scope.preview = function(child) {
+                $scope.child = child
+                $('#modalChild').modal({
+                    keyboard: false,
+                });
+            };
 
-			$scope.getStringOfGroupsOfUser = function (){
-				var groupOfuser = $scope.getGroupOfCurrentUser();		
-				var stringForTooltip = "";			
-				stringForTooltip += groupOfuser.name;	
-				groupOfuser.children.forEach( function(group){
-					stringForTooltip += " > " + group.name;			
-				});
-				
-				return stringForTooltip;						
-			};
+            $scope.canAcceptAlert = function(child) {
+                if (!child) return false;
+                if (!child.requires_address_update) return true;
+                return child.alert &&
+                    child.alert.place_address &&
+                    (child.alert.place_address.trim().length > 0) &&
+                    child.alert.place_neighborhood &&
+                    (child.alert.place_neighborhood.trim().length > 0);
+            };
 
-			$scope.accept = function(child) {
-				if(!$scope.canAcceptAlert(child)) {
-					return;
-				}
+            $scope.getStringOfGroupsOfUser = function() {
+                var groupOfuser = $scope.getGroupOfCurrentUser();
+                var stringForTooltip = "";
+                stringForTooltip += groupOfuser.name;
+                groupOfuser.children.forEach(function(group) {
+                    stringForTooltip += " > " + group.name;
+                });
 
-				$scope.sendingAlert = true;
+                return stringForTooltip;
+            };
 
-				Alerts.accept({id: child.id, place_address: child.alert.place_address, place_neighborhood: child.alert.place_neighborhood}, function() {
-					$scope.refresh();
-					$scope. child = {};
-					$('#modalChild').modal('hide');
-					$scope.sendingAlert = false;
-				});
-			};
-
-			$scope.reject = function(child) {
-				Alerts.reject({id: child.id}, function() {
-					$scope.refresh();
-					$scope.child = {};
-					$('#modalChild').modal('hide');
-				});
-			};
-			
-			Platform.whenReady(function() {
-				$scope.causes = StaticData.getAlertCauses();
-				if($scope.causes_filter.length == 0){
-                    Object.values($scope.causes).forEach(val => $scope.causes_filter.push(({value: val.id, displayName: val.label})));
-                    $scope.causes_filter.sort((a,b) => (a.displayName > b.displayName) ? 1 : ((b.displayName > a.displayName) ? -1 : 0))		
+            $scope.accept = function(child) {
+                if (!$scope.canAcceptAlert(child)) {
+                    return;
                 }
-				$scope.refresh();
-			});
 
-		});
+                $scope.sendingAlert = true;
+
+                Alerts.accept({ id: child.id, place_address: child.alert.place_address, place_neighborhood: child.alert.place_neighborhood }, function() {
+                    $scope.refresh();
+                    $scope.child = {};
+                    $('#modalChild').modal('hide');
+                    $scope.sendingAlert = false;
+                });
+            };
+
+            $scope.reject = function(child) {
+                Alerts.reject({ id: child.id }, function() {
+                    $scope.refresh();
+                    $scope.child = {};
+                    $('#modalChild').modal('hide');
+                });
+            };
+
+            Platform.whenReady(function() {
+                $scope.causes = StaticData.getAlertCauses();
+                if ($scope.causes_filter.length == 0) {
+                    Object.values($scope.causes).forEach(val => $scope.causes_filter.push(({ value: val.id, displayName: val.label })));
+                    $scope.causes_filter.sort((a, b) => (a.displayName > b.displayName) ? 1 : ((b.displayName > a.displayName) ? -1 : 0))
+                }
+                $scope.selectedGroup = Identity.getCurrentUser().group;
+                $scope.refresh();
+            });
+
+        });
 
 })();
