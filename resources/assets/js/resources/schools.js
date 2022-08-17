e397f5021e3a41d5fcbcae3e0861042eacdb713e
Rafael Emerick <rcorreia@unicef.org>
2022-08-17 14:40:34 -0300

    adicionando cadastro de escola para gestor nacional

--- resources/assets/js/resources/schools.js
+++ resources/assets/js/resources/schools.js
@@ -5,9 +5,10 @@
 
 			var headers = API.REQUIRE_AUTH;
 		
-			return $resource(API.getURI('schools/:id'), {id: '@id'}, {
+			return $resource(API.getURI('schools/:id'), {id: '@id', with: '@with'}, {
 
 				find: {method: 'GET', headers: headers},
+				save: {method: 'POST', headers: headers},
 				search: {url: API.getURI('schools/search'), method: 'POST', headers: headers}, 
 				getById: {url: API.getURI('schools/public'), method: 'GET'},
 				all_educacenso: {url: API.getURI('schools/all_educacenso'), method: 'GET', headers: headers},
