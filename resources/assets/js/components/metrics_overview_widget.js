7bceae0e7ccd843cce380bc6887301c55a4a3518
manoelfilho <manoel.tsf@gmail.com>
2023-04-13 16:29:17 -0300

    Adequando o painel metrics_overview_widget para leitura correta dos campos oriundos do cache do servidor

--- resources/assets/js/components/metrics_overview_widget.js
+++ resources/assets/js/components/metrics_overview_widget.js
@@ -1,6 +1,6 @@
-(function() {
+(function () {
 
-    angular.module('BuscaAtivaEscolar').directive('metricsOverview', function(moment, Platform, Reports, Report, Charts, Identity) {
+    angular.module('BuscaAtivaEscolar').directive('metricsOverview', function (moment, Platform, Reports, Report, Charts, Identity) {
 
         function init(scope, element, attrs) {
 
@@ -9,31 +9,30 @@
             function refreshMetrics() {
 
                 if (attrs.ibgeId && attrs.uf) {
-                    Report.getStatusCityByCountry({ ibge_id: attrs.ibgeId, uf: attrs.uf }, function(data) {
-                        metrics = data._data;
+                    Report.getStatusCityByCountry({ ibge_id: attrs.ibgeId, uf: attrs.uf }, function (data) {
+                        metrics = data.stats;
                         metrics.cases._alteration = parseInt(metrics.cases._interrupted) + parseInt(metrics.cases._transferred);
                     });
                 } else {
-                    Report.getStatusCity({ city: Identity.getCurrentUser().tenant.city.name, uf: Identity.getCurrentUser().tenant.city.uf }, function(data) {
-                        metrics = data._data;
+                    Report.getStatusCity({ city: Identity.getCurrentUser().tenant.city.name, uf: Identity.getCurrentUser().tenant.city.uf }, function (data) {
+                        metrics = data.stats;
                         metrics.cases._alteration = parseInt(metrics.cases._interrupted) + parseInt(metrics.cases._transferred);
                     });
                 }
-
             }
 
-            scope.getMetrics = function() {
+            scope.getMetrics = function () {
                 return metrics;
             };
 
-            Platform.whenReady(function() {
+            Platform.whenReady(function () {
                 refreshMetrics();
             });
 
-            scope.$watch('objectToInjectInMetrics', function(value) {
+            scope.$watch('objectToInjectInMetrics', function (value) {
                 if (value) {
                     scope.Obj = value;
-                    scope.Obj.invoke = function(ibgeId) {
+                    scope.Obj.invoke = function (ibgeId) {
                         attrs.ibgeId = ibgeId;
                         refreshMetrics();
                     }
