dafdbccd12f8fc1185e427cc38ef4ff85e1fd00e
Ricardo Reichert <101797420+ricardosreichert@users.noreply.github.com>
2023-07-07 14:48:55 +0200

    UNICEF01/BuscaAtiva#1408 (#769)
    * Melhoria na tabela de importacao
    * Melhoria no design do modal de importacao
    * Ajustes no upload de arquivos

--- resources/assets/js/modals/file_uploader.js
+++ resources/assets/js/modals/file_uploader.js
@@ -1,56 +1,69 @@
-(function() {
+(function () {
+  angular
+    .module('BuscaAtivaEscolar')
+    .controller(
+      'FileUploaderModalCtrl',
+      function FileUploaderModalCtrl(
+        $scope,
+        $uibModalInstance,
+        API,
+        StaticData,
+        Upload,
+        uploadUrl,
+        uploadParameters,
+        title,
+        message
+      ) {
+        $scope.title = title;
+        $scope.message = message;
+        $scope.allowedMimeTypes = StaticData.getAllowedMimeTypes().join(',');
 
-    angular
-        .module('BuscaAtivaEscolar')
-        .controller('FileUploaderModalCtrl', function FileUploaderModalCtrl($scope, $uibModalInstance, API, StaticData, Upload, uploadUrl, uploadParameters, title, message) {
+        $scope.file = null;
+        $scope.progress = 0;
+        $scope.uploadingFile = false;
 
+        $scope.upload = function (file) {
+          if (!uploadParameters) uploadParameters = {};
+          uploadParameters.file = file;
 
+          $scope.uploadingFile = true;
 
-            $scope.title = title;
-            $scope.message = message;
-            $scope.allowedMimeTypes = StaticData.getAllowedMimeTypes().join(",");
+          Upload.upload({
+            url: uploadUrl,
+            data: uploadParameters,
+            headers: API.REQUIRE_AUTH,
+          }).then(onSuccess, onError, onProgress);
+        };
 
-            $scope.file = null;
-            $scope.progress = 0;
-            $scope.isUploading = false;
+        function onSuccess(res) {
+          if (!res.data) return onError(res);
 
+          $uibModalInstance.close({ response: res.data });
+          $scope.uploadingFile = false;
+        }
 
-            $scope.upload = function(file) {
-                if (!uploadParameters) uploadParameters = {};
-                uploadParameters.file = file;
+        function onError(res) {
+          console.error(
+            '[modal.file_uploader] Error when uploading: ',
+            res.status,
+            'Response: ',
+            res
+          );
+          $scope.uploadingFile = false;
+        }
 
-                $scope.isUploading = true;
+        function onProgress(ev) {
+          $scope.progress = ev.loaded / ev.total;
+        }
 
-                Upload.upload({ url: uploadUrl, data: uploadParameters, headers: API.REQUIRE_AUTH }).then(onSuccess, onError, onProgress);
+        $scope.calculateProgressWidth = function () {
+          return parseInt(100.0 * $scope.progress) + '%';
+        };
 
-            };
-
-            function onSuccess(res) {
-                if (!res.data) return onError(res);
-
-
-                $uibModalInstance.close({ response: res.data });
-                $scope.isUploading = false;
-            }
-
-            function onError(res) {
-                console.error('[modal.file_uploader] Error when uploading: ', res.status, 'Response: ', res);
-                $scope.isUploading = false;
-            }
-
-            function onProgress(ev) {
-                $scope.progress = (ev.loaded / ev.total);
-            }
-
-            $scope.calculateProgressWidth = function() {
-                return parseInt(100.0 * $scope.progress) + "%";
-            };
-
-            $scope.close = function() {
-                $scope.isUploading = false;
-                $uibModalInstance.dismiss(false);
-            }
-
-        });
-
+        $scope.close = function () {
+          $scope.uploadingFile = false;
+          $uibModalInstance.dismiss(false);
+        };
+      }
+    );
 })();
