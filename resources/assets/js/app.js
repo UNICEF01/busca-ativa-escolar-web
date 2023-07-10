0c646f8d3b2f477c6eae9966d5a328945a694319
Ricardo Reichert <rreichert@unicef.org>
2023-07-10 21:50:23 +0200

    Ajuste no Modal de Desativacao de Users

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,32 +1,28 @@
-(function() {
-    identify('core', 'app.js');
+(function () {
+  identify('core', 'app.js');
 
-    angular
-        .module('BuscaAtivaEscolar', [
-            'ngToast',
-            'ngAnimate',
-            'ngCookies',
-            'ngResource',
-            'ngStorage',
-            'ngFileUpload',
-
-            'BuscaAtivaEscolar.Config',
-
-            'angularMoment',
-            'highcharts-ng',
-            'checklist-model',
-
-            'idf.br-filters',
-            'jsonFormatter',
-            'ui.router',
-            'ui.bootstrap',
-            'ui.select',
-            'ui.utils.masks',
-            'ui.ace',
-            'datatables',
-            'ui.mask',
-            'angular.viacep',
-            'ngclipboard',
-            'ng-fusioncharts'
-        ])
+  angular.module('BuscaAtivaEscolar', [
+    'ngToast',
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngStorage',
+    'ngFileUpload',
+    'BuscaAtivaEscolar.Config',
+    'angularMoment',
+    'highcharts-ng',
+    'checklist-model',
+    'idf.br-filters',
+    'jsonFormatter',
+    'ui.router',
+    'ui.bootstrap',
+    'ui.select',
+    'ui.utils.masks',
+    'ui.ace',
+    'datatables',
+    'ui.mask',
+    'angular.viacep',
+    'ngclipboard',
+    'ng-fusioncharts',
+  ]);
 })();
--- resources/assets/js/modals/confirm.js
+++ resources/assets/js/modals/confirm.js
@@ -1,21 +1,32 @@
-(function() {
+(function () {
+  angular
+    .module('BuscaAtivaEscolar')
+    .controller(
+      'ConfirmModalCtrl',
+      function ConfirmModalCtrl(
+        $scope,
+        $uibModalInstance,
+        message,
+        details,
+        canDismiss,
+        htmlDetails
+      ) {
+        $scope.message = message;
+        $scope.details = details;
+        $scope.canDismiss = canDismiss;
 
-    angular
-        .module('BuscaAtivaEscolar')
-        .controller('ConfirmModalCtrl', function ConfirmModalCtrl($scope, $uibModalInstance, message, details, canDismiss) {
+        if (htmlDetails) {
+          $scope.htmlDetails = htmlDetails;
+          $scope.details = null;
+        }
 
-            $scope.message = message;
-            $scope.details = details;
-            $scope.canDismiss = canDismiss;
+        $scope.agree = function () {
+          $uibModalInstance.close(true);
+        };
 
-            $scope.agree = function() {
-                $uibModalInstance.close(true);
-            };
-
-            $scope.disagree = function() {
-                $uibModalInstance.dismiss(false);
-            };
-
-        });
-
+        $scope.disagree = function () {
+          $uibModalInstance.dismiss(false);
+        };
+      }
+    );
 })();
