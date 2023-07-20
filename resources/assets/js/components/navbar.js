d7ba6a34f7f32d8982915168150831ed2bb8fea8
Ricardo Reichert <rreichert@unicef.org>
2023-07-20 16:43:58 +0200

    Removido Servico de Notificacoes

--- resources/assets/js/components/navbar.js
+++ resources/assets/js/components/navbar.js
@@ -1,81 +1,90 @@
-(function() {
-
-    angular.module('BuscaAtivaEscolar').directive('appNavbar', function($location, $state, Identity, StaticData, Notifications, Platform, Auth, Children) {
-
+(function () {
+  angular
+    .module('BuscaAtivaEscolar')
+    .directive(
+      'appNavbar',
+      function (
+        $location,
+        $state,
+        Identity,
+        StaticData,
+        Platform,
+        Auth,
+        Children
+      ) {
         function init(scope) {
+          scope.identity = Identity;
+          scope.auth = Auth;
+          scope.pending_requests = 0;
 
-            scope.identity = Identity;
-            scope.auth = Auth;
-            scope.notifications = Notifications;
-            scope.pending_requests = 0;
+          scope.location = $location.url();
+          scope.state = window.location.pathname;
 
-            scope.showNotifications = true;
+          scope.isHidden = function () {
+            return !!Platform.getFlag('HIDE_NAVBAR');
+          };
 
-            scope.location = $location.url();
-            scope.state = window.location.pathname;
+          scope.renderTenantName = function () {
+            if (Identity.getCurrentUser().tenant)
+              return Identity.getCurrentUser().tenant.name;
+            if (Identity.getCurrentUser().uf)
+              return StaticData.getCurrentUF().name;
+            return '';
+          };
 
-
-            scope.isHidden = function() {
-                return !!Platform.getFlag('HIDE_NAVBAR');
-            };
-
-            scope.renderTenantName = function() {
-                if (Identity.getCurrentUser().tenant) return Identity.getCurrentUser().tenant.name;
-                if (Identity.getCurrentUser().uf) return StaticData.getCurrentUF().name;
-                return '';
-            };
-
-            scope.onMenuToggle = function(isOpen) {
-                if (isOpen) Notifications.refresh();
-            };
-
-            scope.toggleNotifications = function($event) {
-                scope.showNotifications = !scope.showNotifications;
-
-                $event.stopPropagation();
-                $event.stopImmediatePropagation();
-                $event.preventDefault();
-
-                return false;
-            };
-
-            if (Identity.isLoggedIn()) {
-
-                if (Identity.isUserType("supervisor_institucional") || Identity.isUserType("coordenador_operacional")) {
-
-                    Children.requests().$promise.then(function(value) {
-                        value.data.forEach(function(request) {
-                            if (Identity.isUserType("supervisor_institucional") && Identity.getCurrentUserID() === request.requester_id && request.status === "requested") {
-                                scope.pending_requests += 1;
-                            }
-                            if (Identity.isUserType("coordenador_operacional") && request.status == "requested") {
-                                scope.pending_requests += 1;
-                            }
-                        });
-                    });
-                }
+          if (Identity.isLoggedIn()) {
+            if (
+              Identity.isUserType('supervisor_institucional') ||
+              Identity.isUserType('coordenador_operacional')
+            ) {
+              Children.requests().$promise.then(function (value) {
+                value.data.forEach(function (request) {
+                  if (
+                    Identity.isUserType('supervisor_institucional') &&
+                    Identity.getCurrentUserID() === request.requester_id &&
+                    request.status === 'requested'
+                  ) {
+                    scope.pending_requests += 1;
+                  }
+                  if (
+                    Identity.isUserType('coordenador_operacional') &&
+                    request.status == 'requested'
+                  ) {
+                    scope.pending_requests += 1;
+                  }
+                });
+              });
             }
+          }
 
-            Platform.whenReady(function() {
-                //verify signature LGPD
-                if (!Identity.getCurrentUser().lgpd) {
-                    return $state.go('lgpd_signup', { id: Identity.getCurrentUser().id });
-                }
-            });
-
-            scope.canSeeConfigPage = function() {
-                if ((scope.identity.can('settings.manage') || scope.identity.can('groups.manage') || scope.identity.manageImports()) &&
-                    (scope.identity.getCurrentUser().tenant_id && scope.identity.getCurrentUser().group.is_primary)) { return true; }
-                return false;
+          Platform.whenReady(function () {
+            //verify signature LGPD
+            if (!Identity.getCurrentUser().lgpd) {
+              return $state.go('lgpd_signup', {
+                id: Identity.getCurrentUser().id,
+              });
             }
+          });
 
+          scope.canSeeConfigPage = function () {
+            if (
+              (scope.identity.can('settings.manage') ||
+                scope.identity.can('groups.manage') ||
+                scope.identity.manageImports()) &&
+              scope.identity.getCurrentUser().tenant_id &&
+              scope.identity.getCurrentUser().group.is_primary
+            ) {
+              return true;
+            }
+            return false;
+          };
         }
 
         return {
-            link: init,
-            replace: true,
-            templateUrl: '/views/navbar.html'
+          link: init,
+          replace: true,
+          templateUrl: '/views/navbar.html',
         };
-    });
-
+      }
+    );
 })();
