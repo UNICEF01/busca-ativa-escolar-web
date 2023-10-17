625af0f28f07e17e9476f76c9a8043ecb8c8b3b9
Ricardo Reichert <101797420+ricardosreichert@users.noreply.github.com>
2023-10-17 21:48:11 +0200

    Unicef01/busca ativa#1481 (#782)
    * UNICEF01/BuscaAtiva#1482
    * Adicionado Filtro de casos em atraso
    
    UNICEF01/BuscaAtiva#1483
    + Adicionado Filtro de casos em atraso na tabela de casos
    + Ajustado Nav Bar para nao quebrar linha quando nome for grande
    
    * Correcao no download do painel
    UNICEF01/BuscaAtiva#1485
    
    * Adicioando Campo para Prazo na Gestao do Caso
    
    UNICEF01/BuscaAtiva#1484
    
    ---------
    
    Co-authored-by: manoelfilho <manoel.tsf@gmail.com>

--- resources/assets/js/settings/manage_deadlines.js
+++ resources/assets/js/settings/manage_deadlines.js
@@ -1,38 +1,33 @@
-(function() {
+(function () {
+  angular
+    .module('BuscaAtivaEscolar')
+    .controller(
+      'ManageDeadlinesCtrl',
+      function ($scope, ngToast, Platform, Tenants, StaticData) {
+        $scope.static = StaticData;
+        $scope.tenantSettings = {};
 
-    angular.module('BuscaAtivaEscolar')
-        .controller('ManageDeadlinesCtrl', function($scope, ngToast, Platform, Tenants, StaticData) {
+        $scope.save = function () {
+          Tenants.updateSettings($scope.tenantSettings).$promise.then(
+            function () {
+              ngToast.success('Configurações salvas com sucesso!');
+              $scope.refresh();
+            },
+            function () {
+              ngToast.danger('Ocorreu um erro ao atualizar as configurações');
+            }
+          );
+        };
 
-            $scope.static = StaticData;
-            $scope.tenantSettings = {};
+        $scope.refresh = function () {
+          Tenants.getSettings(function (res) {
+            $scope.tenantSettings = res;
+          });
+        };
 
-            $scope.save = function() {
-
-                Tenants.updateSettings($scope.tenantSettings).$promise.then(
-                    function() {
-
-                        ngToast.success('Configurações salvas com sucesso!');
-                        $scope.refresh();
-                    },
-                    function() {
-
-                        ngToast.danger('Ocorreu um erro ao atualizar as configurações');
-                    }
-                );
-
-            };
-
-            $scope.refresh = function() {
-                Tenants.getSettings(function(res) {
-
-                    $scope.tenantSettings = res;
-                });
-            };
-
-            Platform.whenReady(function() {
-                $scope.refresh();
-            })
-
+        Platform.whenReady(function () {
+          $scope.refresh();
         });
-
+      }
+    );
 })();
